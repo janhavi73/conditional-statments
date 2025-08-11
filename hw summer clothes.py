a3465963ee4eb0a15e1a88cca2c03f1d6058e500
janhavi73 <janhavisinghania0607@gmail.com>
2025-08-11 19:27:29 +0100

    Add files via upload

---
+++ hw summer clothes.py
@@ -0,0 +1,11 @@
+temperature=int(input("please enter the temperature at the moment in celcius"))
+if temperature >= 20 and temperature<=10:
+    print("you can wear light clothes")
+if temperature >10 and temperature<5:
+    print("bring a light jumper to put over your clothes")
+if temperature >=5:
+    print("consider bringing a jacket and wearing slightly warmer clothes") 
+else:
+    print("you need to be fully covered- its quite cold!")           
+
+
